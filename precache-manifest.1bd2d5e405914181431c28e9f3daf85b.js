a7a148a9c37599db0da22b86f462b04e8d669d40
Francesca F <fodfran@gmail.com>
2019-11-09 03:54:21 -0500

    Updates

---
+++ precache-manifest.1bd2d5e405914181431c28e9f3daf85b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf29a56590eb7b0a08b6ffcc579c7369",
+    "url": "/tomocats/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/tomocats/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "afa3238937117cd849c4",
+    "url": "/tomocats/static/css/main.8a5ef8ae.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/tomocats/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "afa3238937117cd849c4",
+    "url": "/tomocats/static/js/main.2f1eb5a3.chunk.js"
+  },
+  {
+    "revision": "fb09a9c6185f8ab57e8c",
+    "url": "/tomocats/static/js/runtime-main.132d9561.js"
+  },
+  {
+    "revision": "8c4dfdf52182e425f1220a621ed8a2ba",
+    "url": "/tomocats/static/media/CATCAT__.8c4dfdf5.woff"
+  },
+  {
+    "revision": "cccc18d85c0a444a64aa9feef42c5fa9",
+    "url": "/tomocats/static/media/Kitty face.cccc18d8.woff"
+  }
+]);
